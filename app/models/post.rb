a8202a5f5f94bfcae3c190ebaf8277f3e9f459e6
JY <james.yang@ackama.com>
2020-02-03 15:02:23 +1300

    Expanded Encapsulation Through Our Models

--- app/models/post.rb
+++ app/models/post.rb
@@ -9,7 +9,27 @@ class Post
     @created_at = attributes['created_at']
   end
 
+  def set_attributes(attributes)
+    @id = attributes['id'] if new_record?
+    @title = attributes['title']
+    @body = attributes['body']
+    @author = attributes['author']
+    @created_at ||= attributes['created_at']
+  end
+
+  def new_record?
+    id.nil?
+  end
+
   def save
+    if new_record?
+      insert
+    else
+      update
+    end
+  end
+
+  def insert
     insert_query = <<-SQL
       INSERT INTO posts (title, body, author, created_at)
       VALUES (?, ?, ?, ?)
@@ -22,9 +42,37 @@ class Post
                        Date.current.to_s
   end
 
+  def update
+    update_query = <<-SQL
+      UPDATE posts
+      SET title      = ?,
+          body       = ?,
+          author     = ?
+      WHERE posts.id = ?
+    SQL
+
+    connection.execute update_query,
+                       title,
+                       body,
+                       author,
+                       id
+  end
+
+  # used in application#delete_post
+  def destroy
+    connection.execute "DELETE FROM posts WHERE posts.id = ?", id
+  end
+
   def self.find(id)
     post_hash = connection.execute("SELECT * FROM posts WHERE posts.id = ? LIMIT 1", id).first
     Post.new(post_hash)
+  end
+
+  def self.all
+    post_hashes = connection.execute("SELECT * FROM posts")
+    post_hashes.map do |post_hash|
+      Post.new(post_hash)
+    end
   end
 
   def self.connection
